f264ae5296af523d27049fa61a5f3edb3a1ccc96
Meltime <50629110+qjaxoddl522@users.noreply.github.com>
2025-05-17 23:08:19 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9484 ms, Memory: 30840 KB -BaekjoonHub

---
+++ Python/백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,11 @@
+import sys
+
+arr = [0]*10001
+
+for _ in range(int(sys.stdin.readline())):
+    arr[int(sys.stdin.readline())] += 1
+
+for i in range(10001):
+    if arr[i] != 0:
+        for _ in range(arr[i]):
+            print(i)
