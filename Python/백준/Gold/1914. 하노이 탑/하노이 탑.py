285e2f64cd4ce0ba5f2f42a8e121063aaddf5b5a
Meltime <50629110+qjaxoddl522@users.noreply.github.com>
2025-10-22 15:35:54 +0900

    [Gold V] Title: 하노이 탑, Time: 836 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Gold/1914. 하노이 탑/하노이 탑.py
@@ -0,0 +1,18 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+
+def main():
+    def hanoi(n, start, mid, end):
+        if n == 1:
+            print(start, end)
+            return
+        hanoi(n-1, start, end, mid)
+        print(start, end)
+        hanoi(n-1, mid, start, end)
+
+    N = int(input())
+    print(pow(2, N)-1)
+    if N <= 20:
+        hanoi(N, 1, 2, 3)
+
+main()
